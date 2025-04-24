fa3f164998ef482f4a09319d8a5d0d22bd7d2902
Brendan McCall <brendanmccall3@gmail.com>
2025-04-24 16:38:54 -0600

    Add kotlinEdge

---
+++ src/main/kotlin/kotlinPolygonComponents/kotlinEdge.kt
@@ -0,0 +1,64 @@
+package kotlinPolygonComponents
+
+class kotlinEdge {
+
+    private var startingPoint: kotlinPoint? = null
+    private var endingPoint: kotlinPoint? = null
+
+    fun kotlinEdge(startingPoint: kotlinPoint?, endingPoint: kotlinPoint?): kotlinEdge {
+        this.startingPoint = startingPoint
+        this.endingPoint = endingPoint
+
+        return this
+    }
+
+    override fun toString(): String {
+        return "Edge: $startingPoint, $endingPoint"
+    }
+
+    fun getStartingPoint(): kotlinPoint? {
+        return startingPoint
+    }
+
+    fun getEndingPoint(): kotlinPoint? {
+        return endingPoint
+    }
+
+    fun setStartingPoint(startingPoint: kotlinPoint?) {
+        this.startingPoint = startingPoint
+    }
+
+    fun setEndingPoint(endingPoint: kotlinPoint?) {
+        this.endingPoint = endingPoint
+    }
+
+    fun isConnectedEdge(point: kotlinPoint?): Boolean {
+        return point === startingPoint || point === endingPoint
+    }
+
+    fun getConnectedPoints(connectedEdges: MutableList<kotlinEdge>): MutableList<kotlinPoint?> {
+        val connectedPoints: MutableList<kotlinPoint?> = ArrayList<kotlinPoint?>()
+        val edgeA: kotlinEdge = connectedEdges.first()
+        val edgeB: kotlinEdge = connectedEdges.last()
+
+        if (edgeA.startingPoint === edgeB.startingPoint) {
+            connectedPoints.add(edgeA.endingPoint)
+            connectedPoints.add(edgeB.endingPoint)
+        } else if (edgeA.endingPoint === edgeB.endingPoint) {
+            connectedPoints.add(edgeA.startingPoint)
+            connectedPoints.add(edgeB.startingPoint)
+        } else if (edgeA.startingPoint === edgeB.endingPoint) {
+            connectedPoints.add(edgeA.endingPoint)
+            connectedPoints.add(edgeB.startingPoint)
+        } else if (edgeA.endingPoint === edgeB.startingPoint) {
+            connectedPoints.add(edgeA.startingPoint)
+            connectedPoints.add(edgeB.endingPoint)
+        }
+        return connectedPoints
+    }
+
+    fun flipKotlinEdge(connectedEdges: MutableList<kotlinEdge>): kotlinEdge {
+        val connectedPoints = getConnectedPoints(connectedEdges)
+        return kotlinEdge(connectedPoints.first(), connectedPoints.last())
+    }
+}
